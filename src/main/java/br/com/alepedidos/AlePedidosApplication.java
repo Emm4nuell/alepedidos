9544c4474deed28c7c534b9970b53dc81db11d96
Emm4nuell <eduardo.eesf@gmail.com>
2025-09-18 22:44:34 -0300

    first commit

---
+++ src/main/java/br/com/alepedidos/AlePedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alepedidos/AlePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
